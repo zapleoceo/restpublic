25ed2d0b05e4ba2c1ece53f9d3ff671e84f813c9
TG3 Developer <developer@tg3.com>
2025-09-04 19:57:05 +0700

    Исправление стилей React приложения: обновление vite.config.js и index.html для корректной сборки CSS/JS файлов

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
+  }
 })
