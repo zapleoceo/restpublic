78a8be9b47e0d6fc0f1b7cbbcab81631672a1432
TG3 Developer <developer@tg3.com>
2025-09-06 01:25:24 +0700

    Remove VersionInfo component that was showing debug info on website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MenuPage from './pages/MenuPage';
-import VersionInfo from './components/VersionInfo';
 import './App.css';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <VersionInfo />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/menu" element={<MenuPage />} />
