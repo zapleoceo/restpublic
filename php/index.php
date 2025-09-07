d8bb2a31a620cb2c191da3b633c7cd89b2853e6c
TG3 Developer <developer@tg3.com>
2025-09-07 14:30:35 +0700

    Исправлен путь к MenuCache.php в index.php
    - Изменен путь с '/classes/MenuCache.php' на '/php/classes/MenuCache.php'
    - Исправлена проблема с путями при копировании файла в корень

--- php/index.php
+++ php/index.php
@@ -7,7 +7,7 @@ $productsByCategory = [];
 try {
     require_once __DIR__ . '/vendor/autoload.php';
     if (class_exists('MongoDB\Client')) {
-        require_once __DIR__ . '/classes/MenuCache.php';
+        require_once __DIR__ . '/php/classes/MenuCache.php';
         $menuCache = new MenuCache();
         $menuData = $menuCache->getMenu();
         $categories = $menuData ? $menuData['categories'] : [];
