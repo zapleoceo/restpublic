06df38cce48e5846fbbbb981673ace025d239c48
TG3 Developer <developer@tg3.com>
2025-09-17 22:53:57 +0700

    Fix: убраны якорные ссылки из навигации мини-меню
    - Заменены href="

--- index.php
+++ index.php
@@ -331,8 +331,9 @@ $pageKeywords = $pageMeta['keywords'] ?? '';
                             <?php if (!empty($categories)): ?>
                                 <?php foreach ($categories as $index => $category): ?>
                                     <li>
-                                        <a href="#tab-<?php echo htmlspecialchars($category['category_id']); ?>" 
-                                           class="<?php echo $index === 0 ? 'active' : ''; ?>">
+                                        <a href="#" 
+                                           class="<?php echo $index === 0 ? 'active' : ''; ?>"
+                                           data-tab-id="tab-<?php echo htmlspecialchars($category['category_id']); ?>">
                                             <span><?php echo htmlspecialchars(translateCategoryName($category['category_name'] ?? $category['name'] ?? 'Без названия', getCurrentLanguage())); ?></span>
                                             <svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero"/></svg>
                                         </a>
@@ -558,8 +559,9 @@ $pageKeywords = $pageMeta['keywords'] ?? '';
                     this.classList.add('active'); // Добавляем класс active
                     
                     // Change tab panel visibility (как в исходнике)
+                    const targetTabId = this.getAttribute('data-tab-id');
                     menuItems.forEach(function(panel, index) {
-                        if (index != linkIndex) {
+                        if (panel.id !== targetTabId) {
                             panel.setAttribute('aria-hidden', 'true');
                             panel.removeAttribute('data-tab-active');
                         } else {
